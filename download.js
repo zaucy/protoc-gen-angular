a05b181506b3a7c3caf15ed66450b906b03c441a
zaucy <ezekiel@robotoppression.com>
2018-10-02 20:03:39 -0700

    Added vsts ci

--- download.js
+++ download.js
@@ -4,17 +4,16 @@ const progressDownload = require("progress-download");
 
 const PACKAGE_JSON_PATH = path.resolve(__dirname, 'package.json');
 const PLATFORM = process.platform;
-const ARCH = process.arch;
 const EXTNAME = PLATFORM == 'win32' ? '.exe' : '';
 const PLUGIN_PATH = path.resolve(
-  __dirname, 'release', `protoc-gen-angular-${PLATFORM}-${ARCH}${EXTNAME}`
+  __dirname, 'release', `protoc-gen-angular-${PLATFORM}${EXTNAME}`
 );
 
 if(!fs.existsSync(PLUGIN_PATH)) {
   const {version} = JSON.parse(fs.readFileSync(PACKAGE_JSON_PATH, 'utf8'));
   const PLUGIN_DOWNLOAD_URL = 
     `https://github.com/zaucy/protoc-gen-angular/releases/download/` +
-    `v${version}/protoc-gen-angular-${PLATFORM}-${ARCH}${EXTNAME}`;
+    `v${version}/protoc-gen-angular-${PLATFORM}${EXTNAME}`;
 
   progressDownload(PLUGIN_DOWNLOAD_URL, path.dirname(PLUGIN_PATH))
     .catch(err => {
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const child_process = require("child_process");
-const fs = require("fs");
 const path = require("path");
-const progressDownload = require("progress-download");
 
-const PACKAGE_JSON_PATH = path.resolve(__dirname, 'package.json');
 const PLATFORM = process.platform;
-const ARCH = process.arch;
 const EXTNAME = PLATFORM == 'win32' ? '.exe' : '';
 const PLUGIN_PATH = path.resolve(
-  __dirname, 'release', `protoc-gen-angular-${PLATFORM}-${ARCH}${EXTNAME}`
+  __dirname, 'release', `protoc-gen-angular-${PLATFORM}${EXTNAME}`
 );
 
 module.exports = function(args, options) {
